01ba64bd25259885b26df3d8f055c5f6d47a0fcd
Jprogsie <89649066+Jprogsie@users.noreply.github.com>
2022-05-05 14:22:28 +0530

    Add files via upload

---
+++ codes/C/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!");
+
+  int apples = 20;
+  int apples2 ;
+  apples2 = 40; 
+
+  // printf("%d\n" , apples + apples2 );// "%d" int , "%f" float , "%c" char
+
+
+
+  return 0;
+}
